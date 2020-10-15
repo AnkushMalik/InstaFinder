dbeb98eed3e4711f88880514e8aa6f810e8c71db
Ankush Malik <ankushmalik631@gmail.com>
2020-10-15 22:25:56 +0530

    handle grabbing button parent element on linkedin

--- javascript/content.js
+++ javascript/content.js
@@ -1,5 +1,30 @@
 chrome.runtime.onMessage.addListener(request => {
     if (request.method ==='pageLoad'){
-        console.log('<<<<<<<<hello there user - 2 !!!!!>>>>>>>')
+        let action_button_parent = document.querySelector('section[id*="ember"] .display-flex div.ember-view[id*="ember"].inline-flex')
+        if(action_button_parent){
+            appendSearchInstaButton(action_button_parent)
+        }
+        console.log('>>>>',chrome.extension.getURL('test.html'))
     }
 })
+
+const searchInstaButton = () => {
+    let node = document.createElement("DIV");
+    node.classList.add('insta_button_container')
+
+    let imgNode = document.createElement('img')
+    imgNode.src = chrome.extension.getURL('images/instagram_icon.png')
+
+    let buttonText = document.createElement('span')
+    buttonText.classList.add('insta_button_text')
+    buttonText.appendChild(document.createTextNode("Find on Instagram"))
+
+    node.append(imgNode)
+    node.append(buttonText)
+
+    return node;
+}
+
+function appendSearchInstaButton(parent){
+    console.log('from append action:', parent, searchInstaButton())
+}
