f2261832835f41aeb58bb53251b3b4e1e53dada1
Ankush Malik <ankushmalik631@gmail.com>
2020-10-18 14:22:26 +0530

    Handle basic styling of the extension

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,4 @@
 const chromeBgPage = chrome.extension.getBackgroundPage()
-const serviceTerms = 'http://privacy.unimania.xyz/service_terms_uploader.pdf'
-const privacyPolicy = 'http://privacy.unimania.xyz/privacy_policy_uploader.pdf'
-const mailTo = 'uploader@unimania.xyz'
 
 const mainContainer = `
 <link rel="stylesheet" href="./stylesheets/index.css">
@@ -9,7 +6,7 @@ const mainContainer = `
     <div class="left-column">
         <button id="nav-back-btn" class="nav-back-btn">
             <span class="arrow_back">
-                back
+                <
             </span>
             <span>Back</span>
         </button>
@@ -30,27 +27,7 @@ const mainContainer = `
             <div id="desc1"></div>
         </div>
     </div>
-    <div class="logo">
-        <div class="add-info">
-            <p><span class="note-info show-first-note">Terms of Use and Privacy Policy</span></p>
-            <p><span class="note-info show-second-note">Copyright Notice</span></p>
-            <p><a class="note-info note-info-href" href="mailto:${mailTo}" target="_self">Contact Us</a></p>
-        </div>
-    </div>
 </div>`
-
-let autoShow = false;
-const aggrement0CookieName = 'desk_agreement_0_'
-const aggrement1CookieName = 'desk_agreement_1_'
-const aggrement2CookieName = 'desk_agreement_2_'
-
-const notif1 = `<div>By using Desktop for Instagram, you agree to our
-                        <a target="_blank" href="${serviceTerms}">Terms of Use</a> and <a target="_blank" href="${privacyPolicy}">Privacy Policy</a>. 
-                        Questions? <a href="mailto:${mailTo}" target="_self">Contact Us</a>!</div> 
-                        <input class="acceptBtnTermsOfUse" type="button" value="I agree">`
-
-const notif2 = `Desktop for Instagram was developed by Unimania Inc. It is not affiliated with or endorsed by Instagram Inc. Instagramâ„¢ is a trademark of Instagram Inc. 
-                        <input class="acceptBtnCopyrightNotice" type="button" value="I agree">`
 
 function getAndSetInstaCookies() {
     const instaFirstCookie = localStorage.getItem('insta_first') ? localStorage.getItem('insta_first') : Date.now()
@@ -65,7 +42,7 @@ async function appStart() {
 
     $(function () {
         function setMobileFrameSize() {
-            var mobileWidth = $(window).outerHeight() * 60 / 100
+            var mobileWidth = $(window).outerHeight() * 75 / 100
             $('.phone').width(mobileWidth);
         }
 
@@ -96,12 +73,13 @@ async function appStart() {
         } else {
             $('#instaframe').show();
         }
-        document.getElementById('nav-back-btn').addEventListener("click", goStepBack);
+        $('#nav-back-btn').on('click',goStepBack)
     })
 }
 
 function goStepBack() {
-    history.back();
+    // $('#').slideDown();
+    $("#instaframe,#nav-back-btn").hide();
 }
 
 function getChromeVersion(){
